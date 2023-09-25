c45b6bbba2c2dcf12210dfb80bc28795f63371f8
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-09-25 11:23:58 -0700

    feat(movie.router): Updating to catch the error with try catch

--- src/routers/genre.router.js
+++ src/routers/genre.router.js
@@ -7,57 +7,77 @@ const { createGenreSchema, updateGenreSchema, getGenreSchema } = require('../sch
 const validatorHandler = require('../middlewares/validator.handler')
 
 router.get('/', async (req, res, next) => {
-  const genres = await genreService.findAll()
-  res.status(200).json({
-    data: genres,
-    message: 'genres listed'
-  })
+  try {
+    const genres = await genreService.findAll()
+    res.status(200).json({
+      data: genres,
+      message: 'genres listed'
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:id',
   validatorHandler(getGenreSchema, 'params'),
   async (req, res, next) => {
-    const { id } = req.params
-    const genre = await genreService.findById(id)
-    res.status(200).json({
-      data: genre,
-      message: 'genre retrieved'
-    })
+    try {
+      const { id } = req.params
+      const genre = await genreService.findById(id)
+      res.status(200).json({
+        data: genre,
+        message: 'genre retrieved'
+      })
+    } catch (error) {
+      next(error)
+    }
   })
 
 router.post('/',
   validatorHandler(createGenreSchema, 'body'),
   async (req, res, next) => {
-    const { body } = req
-    const createdGenre = await genreService.create(body)
-    res.status(201).json({
-      data: createdGenre,
-      message: 'genre created'
-    })
+    try {
+      const { body } = req
+      const createdGenre = await genreService.create(body)
+      res.status(201).json({
+        data: createdGenre,
+        message: 'genre created'
+      })
+    } catch (error) {
+      next(error)
+    }
   })
 
 router.patch('/:id',
   validatorHandler(getGenreSchema, 'params'),
   validatorHandler(updateGenreSchema, 'body'),
   async (req, res, next) => {
-    const { id } = req.params
-    const { body } = req
-    const updatedGenre = await genreService.update(id, body)
-    res.status(200).json({
-      data: updatedGenre,
-      message: 'genre updated'
-    })
+    try {
+      const { id } = req.params
+      const { body } = req
+      const updatedGenre = await genreService.update(id, body)
+      res.status(200).json({
+        data: updatedGenre,
+        message: 'genre updated'
+      })
+    } catch (error) {
+      next(error)
+    }
   })
 
 router.delete('/:id',
   validatorHandler(getGenreSchema, 'params'),
   async (req, res, next) => {
-    const { id } = req.params
-    const deletedGenre = await genreService.delete(id)
-    res.status(200).json({
-      data: deletedGenre,
-      message: 'genre deleted'
-    })
+    try {
+      const { id } = req.params
+      const deletedGenre = await genreService.delete(id)
+      res.status(200).json({
+        data: deletedGenre,
+        message: 'genre deleted'
+      })
+    } catch (error) {
+      next(error)
+    }
   })
 
 module.exports = router
